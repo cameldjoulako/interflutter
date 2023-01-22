0e8789d2aef2e1b8ee18bfb6aa600bba018dd18d
Camel Djoulako <cameldjoulako17@gmail.com>
2023-01-22 19:48:22 +0100

    fix date display

--- interflutter/lib/pages/hero_list.dart
+++ interflutter/lib/pages/hero_list.dart
@@ -15,19 +15,20 @@ class HeroList extends StatefulWidget {
 }
 
 class _HeroListState extends State<HeroList> {
-  String formatDate(BuildContext context, String dateKey) {
-    var date = DateTime.tryParse(AppLocalizations.of(context)?.dateKey);
-    if (date != null) {
-      var formatter =
-          DateFormat.yMMMMd(Localizations.localeOf(context).toString());
-      return formatter.format(date);
-    } else {
-      return "";
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
+    /* var formatter =
+        DateFormat.yMMMMd(Localizations.localeOf(context).toString()); */
+
+    //var date = DateTime.tryParse(AppLocalizations.of(context)!.bornHero1);
+
+    /* DateFormat.yMMMMd(Localizations.localeOf(context).toString())
+        .format(DateTime.tryParse(AppLocalizations.of(context)!.bornHero1)!) */
+    //;
+
+    /* print(DateFormat.yMMMMd(Localizations.localeOf(context).toString())
+        .format(DateTime.tryParse(AppLocalizations.of(context)!.bornHero1)!)); */
+
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.chooseLang),
@@ -58,42 +59,42 @@ class _HeroListState extends State<HeroList> {
                   HeroCard(
                     name: 'Grace Hopper',
                     born:
-                        "${AppLocalizations.of(context)!.bornText} ${formatDate(context, 'bornHero1')}",
+                        "${AppLocalizations.of(context)!.bornText} ${DateFormat.yMMMMd(Localizations.localeOf(context).toString()).format(DateTime.tryParse(AppLocalizations.of(context)!.bornHero1)!)}",
                     bio: AppLocalizations.of(context)!.bioHero1,
                     imagePath: 'assets/images/grace_hopper.jpg',
                   ),
                   HeroCard(
                     name: 'Alan Turing',
                     born:
-                        "${AppLocalizations.of(context)!.bornText} ${formatDate(context, 'bornHero2')}",
+                        "${AppLocalizations.of(context)!.bornText} ${DateFormat.yMMMMd(Localizations.localeOf(context).toString()).format(DateTime.tryParse(AppLocalizations.of(context)!.bornHero1)!)}",
                     bio: AppLocalizations.of(context)!.bioHero2,
                     imagePath: 'assets/images/alan_turing.jpg',
                   ),
                   HeroCard(
                     name: 'Barbara Liskov',
                     born:
-                        "${AppLocalizations.of(context)!.bornText} ${formatDate(context, 'bornHero3')}",
+                        "${AppLocalizations.of(context)!.bornText} ${DateFormat.yMMMMd(Localizations.localeOf(context).toString()).format(DateTime.tryParse(AppLocalizations.of(context)!.bornHero1)!)}",
                     bio: AppLocalizations.of(context)!.bioHero3,
                     imagePath: 'assets/images/barbara_liskov.jpg',
                   ),
                   HeroCard(
                     name: 'Steve Wozniak',
                     born:
-                        "${AppLocalizations.of(context)!.bornText} ${formatDate(context, 'bornHero4')}",
+                        "${AppLocalizations.of(context)!.bornText} ${DateFormat.yMMMMd(Localizations.localeOf(context).toString()).format(DateTime.tryParse(AppLocalizations.of(context)!.bornHero1)!)}",
                     bio: AppLocalizations.of(context)!.bioHero4,
                     imagePath: 'assets/images/steve_wozniak.jpg',
                   ),
                   HeroCard(
                     name: 'Tim Berners-Lee',
                     born:
-                        "${AppLocalizations.of(context)!.bornText} ${formatDate(context, 'bornHero5')}",
+                        "${AppLocalizations.of(context)!.bornText} ${DateFormat.yMMMMd(Localizations.localeOf(context).toString()).format(DateTime.tryParse(AppLocalizations.of(context)!.bornHero1)!)}",
                     bio: AppLocalizations.of(context)!.bioHero5,
                     imagePath: 'assets/images/tim_berners_lee.jpg',
                   ),
                   HeroCard(
                     name: 'Bill Gates',
                     born:
-                        "${AppLocalizations.of(context)!.bornText} ${formatDate(context, 'bornHero6')}",
+                        "${AppLocalizations.of(context)!.bornText} ${DateFormat.yMMMMd(Localizations.localeOf(context).toString()).format(DateTime.tryParse(AppLocalizations.of(context)!.bornHero1)!)}",
                     bio: AppLocalizations.of(context)!.bioHero6,
                     imagePath: 'assets/images/bill_gates.jpg',
                   ),
